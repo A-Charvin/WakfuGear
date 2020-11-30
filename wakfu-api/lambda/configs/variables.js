448fcd3f8b3c6cf84dc84e9e607f831b002f5463
A-Charvin <anandcharvin@yahoo.com>
2020-11-30 09:54:02 +0530

    Update variables.js

--- wakfu-api/lambda/configs/variables.js
+++ wakfu-api/lambda/configs/variables.js
@@ -4,6 +4,7 @@ exports.ALLOWED_ORIGINS = [
   'https://wakfu-gear.netlify.app'
   'https://almostgeared.netlify.app'
   'https://wakfu-api.netlify.app'
+  'https://almost-api.netlify.app'
 ]
 
 exports.endpointWAPI= 'https://wakfu.cdn.ankama.com/gamedata'
